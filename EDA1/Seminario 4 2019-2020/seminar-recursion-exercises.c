9d7c7c61e1b28402fe690e020e152ec78cd1eff4
Marc Pascual Martinez <mpascualm17@gmail.com>
2020-02-24 11:39:40 +0100

    push project

---
+++ EDA1/Seminario 4 2019-2020/seminar-recursion-exercises.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <math.h>
+#include "seminar-recursion-exercises.h"
+
+// Exercise 11
+int vector_sum_subs_rec(int* v1, int* v2, int idx, int size) {
+    return FALSE;
+}
+
+int vector_sum_subs_iter(int* v1, int* v2, int size) {
+    return FALSE;
+}
+
+int vector_sum_subs(int* v1, int* v2, int size) {
+    return FALSE;
+}
+
+// Exercise 12
+int sum_digits(int number) {
+    return FALSE;
+}
+
+// Exercise 13
+int reverse_digits_simple(int number, int acum) {
+    return FALSE;
+}
+
+int reverse_digits_hard(int number) {
+    return FALSE;
+}
+
+int reverse_digits(int number) {
+    return FALSE;
+}
+
+// Exercise 14
+int norm_rec_rec(int* vector, int size, int acum) {
+    return FALSE;
+}
+
+int norm(int* vector, int size) {
+    return FALSE;
+}
+
+// Exercise 15
+int mystery (int n, int result) {
+    if (n<=0) {
+        return result;
+    } else {
+        return mystery(n-1, result + n*n);
+    }
+}
+
+// Exercise 16
+int is_perfect_square_rec(int number, int step) {
+    return FALSE;
+}
+
+int is_perfect_square(int number) {
+    return FALSE;
+}
+
+// Exercise 17
+int free_of_squares_rec(int number, int step) {
+    return FALSE;
+}
+
+int free_of_squares(int number) {
+    return FALSE;
+}
+
+// Exercise 18
+int reverse_and_sum_rec(int number, int depth) {
+    return FALSE;
+}
+
+int reverse_and_sum(int number) {
+    return FALSE;
+}
+
+// Exercise 19
+int binary_search_rec(int* vector, int left, int right, int number) {
+    return FALSE;
+}
+
+int binary_search(int* vector, int size, int number) {
+    return FALSE;
+}
